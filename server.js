18a8fa2f609dde761770b05eadb2a47a787b7321
Josh Carter <joshcarter8400@gmail.com>
2020-10-21 12:16:53 -0400

    added put request route

--- server.js
+++ server.js
@@ -38,6 +38,32 @@ app.get("/api/candidates", (req, res) => {
   });
 });
 
+// Put request for candidates
+app.put("/api/candidate/:id", (req, res) => {
+  const errors = inputCheck(req.body, "party_id");
+
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: "success",
+      data: req.body,
+      changes: this.changes,
+    });
+  });
+});
+
 // Get parties
 app.get("/api/parties", (req, res) => {
   const sql = `SELECT * FROM parties`;
